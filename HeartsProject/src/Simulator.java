5bc3651a4c245e27fb888c156bede09f3f00bd7b
ryangibbons <rjg8@princeton.edu>
2023-12-14 21:11:16 -0500

    got rid of unnecessary imports

--- HeartsProject/src/Simulator.java
+++ HeartsProject/src/Simulator.java
@@ -1,8 +1,3 @@
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
